cc515105751cfb6b3c919e31079fb1393f29594b
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2019-12-03 17:45:40 +0600

    update factory & Seed and Fixed migration error

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\model\Like;
 use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
         //
+
+        'user_id' => function(){
+            return User::all()->random();
+        },
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\model\Reply;
+use App\model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
+
+        'body' => $faker->sentence,
+
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        },
         //
     ];
 });
