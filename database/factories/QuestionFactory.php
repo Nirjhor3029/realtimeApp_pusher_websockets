0bd262e5c2b1bb82a2c5bae99ed4435a4fe65001
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2019-12-03 15:38:10 +0600

    Creating Migration, Model, Controller, Factories

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\model\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2019_12_03_090648_create_replies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade'); //cascade helps to delete these replies with deleting the comment
+            $table->integer('user_id')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
